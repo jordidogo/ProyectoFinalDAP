92d0e5119f70da4be20f3c10bdd3d78386c35bae
pau-regalado <alu0101330174@ull.edu.es>
2024-01-09 00:13:58 +0000

    LineChartStrategy

--- src/main/java/org/example/Tomorroio/view/strategy/LineChartStrategy.java
+++ src/main/java/org/example/Tomorroio/view/strategy/LineChartStrategy.java
@@ -1,4 +1,20 @@
 package org.example.Tomorroio.view.strategy;
 
-public class LineChartStrategy {
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.xy.XYSeriesCollection;
+
+public class LineChartStrategy implements ChartStrategy {
+    @Override
+    public JFreeChart createChart(String chartType, String xAxisLabel, String yAxisLabel, XYSeriesCollection dataset) {
+        return ChartFactory.createTimeSeriesChart(
+                chartType,
+                xAxisLabel,
+                yAxisLabel,
+                dataset,
+                false,
+                true,
+                false
+        );
+    }
 }
