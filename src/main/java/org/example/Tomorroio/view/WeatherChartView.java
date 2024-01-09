74bd5c43d195613017251511d3dc4288091ecf38
pau-regalado <alu0101330174@ull.edu.es>
2024-01-09 00:04:17 +0000

    metodo createAndShow

--- src/main/java/org/example/Tomorroio/view/WeatherChartView.java
+++ src/main/java/org/example/Tomorroio/view/WeatherChartView.java
@@ -6,6 +6,7 @@ import org.example.Tomorroio.model.WeatherData;
 import org.jfree.data.xy.XYSeriesCollection;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class WeatherChartView extends JFrame implements WeatherView {
     private WeatherData weatherData;
@@ -24,6 +25,21 @@ public class WeatherChartView extends JFrame implements WeatherView {
         this.chartStrategy = chartStrategy;
         this.weatherData = weatherData;
         createAndShowUI();
+    }
+
+    private void createAndShowUI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new GridLayout(3, 2));
+
+        // Utilizar las coordenadas establecidas en WeatherData
+        addChartPanel("Temperature", "Time", "Temperature (°C)", convertToXYSeriesCollection(weatherData.fetchDataSeries("temperature")));
+        addChartPanel("Humidity", "Time", "Humidity (%)", convertToXYSeriesCollection(weatherData.fetchDataSeries("humidity"))); // Changed from PrecipitationProbability to Humidity
+        addChartPanel("WindSpeed", "Time", "Wind Speed (m/s)", convertToXYSeriesCollection(weatherData.fetchDataSeries("windSpeed")));
+        addChartPanel("PressureSurfaceLevel", "Time", "Pressure (hPa)", convertToXYSeriesCollection(weatherData.fetchDataSeries("pressureSurfaceLevel")));
+        addChartPanel("CloudCover", "Time", "Cloud Cover", convertToXYSeriesCollection(weatherData.fetchDataSeries("cloudCover")));
+        addChartPanel("WindGust", "Time", "Wind Gust (m/s)", convertToXYSeriesCollection(weatherData.fetchDataSeries("windGust"))); // Added new chart panel
+        pack();
+        setLocationRelativeTo(null);
     }
 
     public void updateChart(String chartType, XYSeriesCollection newDataset) {
