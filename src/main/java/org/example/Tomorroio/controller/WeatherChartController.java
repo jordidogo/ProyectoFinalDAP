160c6e42bf77fcc3b4440a545715579bf8afe365
Adn086 <alu0100417012@ull.edu.es>
2024-01-09 00:02:32 +0000

    "Añadidos DataSet de Temperatura y Humedad al control y al factory"

--- src/main/java/org/example/Tomorroio/controller/WeatherChartController.java
+++ src/main/java/org/example/Tomorroio/controller/WeatherChartController.java
@@ -1,21 +1,38 @@
 package org.example.Tomorroio.controller;
 
-
 import org.example.Tomorroio.model.WeatherData;
 import org.example.Tomorroio.view.WeatherView;
+import org.jfree.data.xy.XYSeriesCollection;
 
 public class WeatherChartController implements WeatherController {
     private WeatherData weatherData;
     private WeatherView chartView;
+    private TemperatureDatasetFactory temperatureDatasetFactory;
+    private HumidityDatasetFactory humidityDatasetFactory;
+    private WindSpeedDatasetFactory windSpeedDatasetFactory;
+    private PressureDatasetFactory pressureDatasetFactory;
+    private CloudCoverDatasetFactory cloudCoverDatasetFactory;
+    private WindGustDatasetFactory windGustDatasetFactory;
 
     public WeatherChartController(
             WeatherData weatherData,
-            WeatherView chartView) {
+            WeatherView chartView,
+            TemperatureDatasetFactory temperatureDatasetFactory,
+            HumidityDatasetFactory humidityDatasetFactory) {
 
         this.weatherData = weatherData;
         this.chartView = chartView;
+        this.temperatureDatasetFactory = temperatureDatasetFactory;
+        this.humidityDatasetFactory = humidityDatasetFactory;
     }
 
-    public void updateCharts() {
+        public void updateCharts() {
+            XYSeriesCollection temperatureDataset = temperatureDatasetFactory.createDataset();
+            XYSeriesCollection humidityDataset = humidityDatasetFactory.createDataset();
+
+
+            chartView.updateChart("Temperature", temperatureDataset);
+            chartView.updateChart("Humidity", humidityDataset);
+
+        }
     }
-}
---
+++ src/main/java/org/example/Tomorroio/model/factory/TemperatureDatasetFactory.java
@@ -0,0 +1,19 @@
+package org.example.Tomorroio.model.factory;
+
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+public class TemperatureDatasetFactory implements DatasetFactory {
+    @Override
+    public XYSeriesCollection createDataset() {
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        XYSeries series = new XYSeries("temperature");
+
+        // Agregar datos a la serie según tus necesidades
+
+        dataset.addSeries(series);
+        return dataset;
+    }
+}
+
+
